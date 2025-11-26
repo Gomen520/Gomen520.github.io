de53900532e71cea7c408251299376799217efce
Gomen520 <jhw_169@163.com>
2025-11-26 23:11:28 +0800

    3.0

--- script.js
+++ script.js
@@ -19,16 +19,8 @@ function displayMt(m){
     var row=[]
     for (var i=0;i<m.length;i++) if (m[i].length>index[i]&&(row.length==0||compareRow(m[i][index[i]].row,row)<0)) row=m[i][index[i]].row
     if (row.length==0) break
-    var row2=[],row3=row.slice()
-    row2.push(1)
-    while (true){
-      while (row3[0]==0) row3.shift()
-      if (row3.length==0) break
-      row2.push(row3.length)
-      row3[0]--
-    }
     mt+='<tr>'
-    mt+='<td align="center" width="80" bgColor="#eee0e0">'+row2+'</td>'
+    mt+='<td align="center" width="80" bgColor="#eee0e0">'+row+'</td>'
     for (var i=0;i<m.length;i++){
       if (m[i].length>index[i]&&compareRow(m[i][index[i]].row,row)==0) mt+='<td align="center" width="80" bgColor="#e0eee0">'+m[i][index[i]++].value+'</td>'
       else mt+='<td align="center" width="80" bgColor="#e0eee0">'+''+'</td>'
@@ -37,51 +29,105 @@ function displayMt(m){
   }
   mt+='</table>'
 }
-function isDimensionLimited(it,d){ // n-Y
-  if (d.length==1&&it.row.length==d[0]&&(it.parent.row.length<d[0]||it.row[0]>it.parent.row[0])) return true
+function isDimensionLimited(it,d){
+  var row=getFootRow(it,d)
+  if (proc(d).length==1&&proc(d)[0]&&row[1]>proc(d)[0]) return true
+  if (d.length==2&&d[0]==0&&d[1]>0&&row.length>d[1]) return true 
   return false
 }
-function compareRow(r1,r2){
-  if (r1.length<r2.length) return -1
-  if (r1.length>r2.length) return 1
-  for (var k=0;k<r1.length;k++){
-    if (r1[k]<r2[k]) return -1
-    if (r1[k]>r2[k]) return 1
-  }
-  return 0
+function proc(d){
+  if (d.length>2&&d[0]==0&&d[1]>0&&divide(d).length>1) return [divide(d)[0].length-1]
+  if (d.length>2&&d[0]==0&&d[1]>0&&divide(d).length==1&&divide(d)[0].length>2) return [divide(d)[0].length-2]
+  return d
 }
-function rowAddition(r1,r2){
-  if (r1.length<r2.length) return r2
-  if (r2.length==0) return r1
-  var f=r1.slice(0,r1.length-r2.length),b=r2.slice()
-  b[0]+=r1[r1.length-r2.length]
-  return f.concat(b)
-}
-function rowDifference(r1,r2){
-  if (r1.length>r2.length) return r1
-  if (compareRow(r1,r2)<=0) return []
-  var i=0,row=[]
-  while (r1[i]==r2[i]) i++
-  row.push(r1[i]-r2[i])
-  for (++i;i<r1.length;i++) row.push(r1[i])
+function rowGenerator(n){
+  var row=[1]
+  for (var i=1;i<n;i++) row.push(0)
   return row
 }
-function calcFootRow(it,d){
-  var row=[1],diff=rowDifference(it.row,it.parent.row).length
-  if (compareRow(d,[0])>0) while (diff--) row.push(0)
-  return rowAddition(it.row,row)
+function divide(d){
+  var dd=d.slice(1),ret=[]
+  for (var i=0;i<dd.length-1;i++) while (dd[i]-->0) ret.push(rowGenerator(dd.length-i))
+  if (dd[dd.length-1]) ret.push([dd[dd.length-1]])
+  return ret
 }
-function getParentCloumn(s,e){
-  for (var i=0;i<s.length;i++) if (s[i].cloumn==e.parent.cloumn) return i
+function merge(d){
+  var ret=rowGenerator(d[0].length)
+  ret[0]-=1
+  for (var i=0;i<d.length-1;i++) ret[ret.length-d[i].length]++
+  ret[ret.length-d[d.length-1].length]+=d[d.length-1][0]
+  ret.unshift(0)
+  return ret
+}
+function compareRow(r1,r2){
+  var i=0
+  for (;i<r1.length&&i<r2.length;i++){
+    if (r1[i]>r2[i]) return 1
+    if (r1[i]<r2[i]) return -1
+  }
+  if (r1.length>i) return 1
+  if (r2.length>i) return -1
+  return 0
+}
+function compareDimension(r1,r2){
+  return (r1.length<=1?1:r1[1])-(r2.length<=1?1:r2[1])
+}
+function rowAddition(r1,r2){
+  if (r2.length<=1||r2[1]==1) return r1.concat(r2)
+  if (r1.length<=1||r1[1]<r2[1]) return r2
+  var i=1
+  while (i++<r1.length) if (r1[i]<r2[1]) break
+  return r1.slice(0,i).concat(r2.slice(1))
+}
+function rowDifference(r1,r2){
+  if (compareRow(r1,r2)<=0) return []
+  if (r1[1]==1) return r1.slice(0,r1.length-r2.length)
+  var i=0
+  for (;i<r1.length&&i<r2.length;i++) if (r1[i]>r2[i]) break
+  if (r1[i]==1) return r1.slice(i)
+  return [1].concat(r1.slice(i))
+}
+function getFootRow(it,d){
+  var row=rowDifference(it.row,it.parent.row)
+  if (row.length==0||d.length==0||d.length==1&&d[0]==0||d.length==2&&d[0]==0||d.length==3&&d[0]==0&&d[1]==1&&d[2]==0) return rowAddition(it.row,[1])
+  if (row.length==1) return rowAddition(it.row,[1,2])
+  return rowAddition(it.row,[1,row[1]+1])
+}
+function setElementRefrence(m){
+  for (var i=0;i<m.length;i++){
+    for (var j=0;j<m[i].length;j++){
+      if (compareRow(m[i][j].row,[1])==0&&m[i][j].value<=1) continue
+      if (compareRow(m[i][j].row,m[i][j].parent.row)==0) m[i][j].ref=m[i][j].parent
+      else m[i][j].ref=m[i][j].head.parent
+    }
+  }
+}
+function setElementNo(m,b){
+  for (var i=0;i<m.length;i++){
+    for (var j=0;j<m[i].length;j++){
+      if (i==b.cloumn) m[i][j].no=j+1
+      else if (i<b.cloumn||(m[i][j].value<=1&&j==0)) m[i][j].no=0
+      else m[i][j].no=m[i][j].ref.no
+    }
+  }
+}
+function getReferenceChain(it){
+  var c=[]
+  while (true){
+    c.unshift(it)
+    if (it.value<=1&&it.row.length==1) break
+    it=it.ref
+  }
+  return c
 }
 function drawMountain(s,d){
   var m=[]
-  s.forEach(e=>{m.push([{value:e.value,row:[0],cloumn:e.cloumn,parent:e.value<=1?{row:[0],cloumn:-1}:m[getParentCloumn(s,e)][0]}])})
+  s.forEach(e=>{m.push([{value:e.value,row:[1],cloumn:e.cloumn,idx:0,parent:e.parent.cloumn<0?{row:[1],cloumn:-1}:m[e.parent.cloumn][0]}])})
   for (var i=0;i<m.length;i++){
     var it=m[i][0]
     while (it.value>1){
       if (isDimensionLimited(it,d)) break
-      it.foot={value:it.value-it.parent.value,row:calcFootRow(it,d),cloumn:it.cloumn,head:it}
+      it.foot={value:it.value-it.parent.value,row:getFootRow(it,d),cloumn:i,idx:it.idx+1,head:it}
       m[i].push(it.foot)
       var p=it.parent
       if ('foot' in p&&compareRow(p.foot.row,it.foot.row)<=0) p=p.foot
@@ -92,196 +138,173 @@ function drawMountain(s,d){
   }
   return m
 }
-function setElementRefrence(m){
-  for (var i=0;i<m.length;i++){
-    for (var j=0;j<m[i].length;j++){
-      if (compareRow(m[i][j].row,[0])==0&&m[i][j].value==1) continue
-      if (compareRow(m[i][j].row,m[i][j].parent.row)==0) m[i][j].ref=m[i][j].parent
-      else m[i][j].ref=m[i][j].head.parent
-      if (m[i][j].ref.value==1) continue
-      m[i][j].ref=m[i][j].ref.ref
-    }
-  }
+function getOds(m){
+  var o=[]
+  m.forEach(e=>{o.push({value:e[e.length-1].value,cloumn:e[0].cloumn,parent:e[e.length-1].value<=1?{row:[1],cloumn:-1}:o[e[e.length-1].parent.cloumn]})})
+  return o
 }
-function copyElement(op,head,max_row,d){
-  while (true){
-    var row=calcFootRow(head,d)
-    if (compareRow(row,max_row)>0) return
-    head.foot={value:head.value,row:row,cloumn:head.cloumn,no:head.no,head:head,parent:head.parent}
-    if ('foot' in head.parent&&compareRow(head.parent.foot.row,head.foot.row)<=0) head.foot.parent=head.foot.parent.foot
-    op.push(head.foot)
-    head=head.foot
-  }
+function getDs(m){
+  var seq=getReferenceChain(m[m.length-1][m[m.length-1].length-1]).map(e=>{return e.row.length<=1?1:e.row[1]})
+  return toSequence(seq)
 }
-function isEqualSequnce(s1,s2){
-  if (s1.length!=s2.length) return false
-  for (var i=0;i<s1.length;i++) if (s1[i].value!=s2[i].value||s1[i].parent.cloumn!=s2[i].parent.cloumn) return false
-  return true
-}
-function setElementNo(m,b){
-  for (var i=0;i<m.length;i++){
-    for (var j=0;j<m[i].length;j++){
-      if (i==b.cloumn) m[i][j].no=j+1
-      else if (i<b.cloumn||(m[i][j].value<=1&&j==0)) m[i][j].no=0
-      else if (m[i][j].value>1&&compareRow(m[i][j].row,m[i][j].parent.row)==0) m[i][j].no=m[i][j].parent.no
-      else m[i][j].no=m[i][j].head.parent.no
-    }
-  }
-}
-function getReferenceChain(it){
-  var c=[]
-  while (true){
-    c.unshift(it)
-    if (it.value<=1&&it.row[0]==0) break
-    it=it.ref
-  }
-  return c
-}
-function getMainDimensionSequence(t){
-  var s=[]
-  t.forEach(e=>{s.push({value:e.row.length,row:[0],cloumn:e.cloumn})})
-  s[s.length-1].value+=1
-  for (var i=0;i<s.length;i++){
-    if (s[i].value<=1) {s[i].parent={row:[0],cloumn:-1};continue}
-    for (var j=i-1;j>=0;j--) if (s[j].value<s[i].value) {s[i].parent=s[j];break}
-  }
-  return s
-}
-function getBootCloumn(s,d){
+function getBootIndex(s,d){
   var m=drawMountain(s,d)
   setElementRefrence(m)
-  var t=m[m.length-1][m[m.length-1].length-2],b=t.parent
-  if (d.length==1&&t.foot.value>1){
-    var o=[]
-    m.forEach(e=>{o.push({value:e[e.length-1].value,row:[0],cloumn:e[0].cloumn,parent:e[e.length-1].value==1?{row:[0],cloumn:-1}:o[e[e.length-1].parent.cloumn]})})
-    return getBootCloumn(o,d)
+  var t=m[m.length-1][m[m.length-1].length-1]
+  if (t.value==1) t=t.head
+  var b=t.parent
+  if (t.value-b.value>1&&proc(d).length==1){
+    var o=getOds(m)
+    var c=getBootIndex(o,d.length==1||divide(d).length==1?d:merge(divide(d).slice(1)))[0]
+    return [c,m[c].length-1]
   }
-  if (t.row.length==b.row.length||d.length<3) return b.cloumn
-  return getBootCloumn(getMainDimensionSequence(getReferenceChain(t)),d)
+  if (compareDimension(b.row,t.row)<0){
+    setElementRefrence(m)
+    var ch=getReferenceChain(t),dd=d
+    if (d[0]==0&&d[1]==0) dd=d.slice(2)
+    var c=ch[getBootIndex(getDs(m),dd)[0]].cloumn
+    return [c,m[c][m[c].length-1].idx]
+  }
+  return [b.cloumn,b.idx]
 }
-function expandElementDimensionSequence(m,b,t,n,d){
-  if (b.row.length==t.row.length||d.length<3) return
-  for (var i=b.cloumn+1;i<m.length;i++){
+function copyElement(m,b,t,it,op,i,d){
+  if (it.value<=1&&it.row.length>1) it.parent=it.head.parent
+  var min_row=[1],max_row=it.row
+  if (it.row.length>1) min_row=getFootRow(op[op.length-1],d)
+  if (it.no>0){
+    var c=m.length-1-it.cloumn+b.cloumn
+    var p=m[c][0]
+    while (true){
+      if ('foot' in p&&p.foot.no<=it.no) p=p.foot
+      else break
+    }
+    max_row=rowAddition(p.row,rowDifference(it.row,m[b.cloumn][it.no-1].row))
+    if ('offset' in it) max_row=rowAddition(p.row,it.offset[i])
+    else if ('foot' in it.ref&&compareRow(it.ref.foot.row,it.row)<=0&&'offset' in it.ref.foot) max_row=rowAddition(p.row,it.ref.foot.offset[i])
+    else if ('offset' in it.ref) max_row=rowAddition(rowAddition(p.row,it.ref.offset[i]),rowDifference(it.row,it.ref.row))
+  }
+  if (d.length==0||d.length==1&&d[0]==0||d.length==2&&d[0]==0||d.length==3&&d[0]==0&&d[1]==1&&d[2]==0) max_row=it.row
+  if (compareRow(min_row,max_row)>0) alert('collapsed!')
+  var row=min_row
+  while (compareRow(row,max_row)<=0){
+    op.push({value:it.value,row:row,cloumn:m.length-1,idx:op.length,no:it.no})
+    if (op.length>1){
+      op[op.length-1].head=op[op.length-2]
+      op[op.length-2].foot=op[op.length-1]
+    }
+    var pc=it.parent.cloumn>=b.cloumn?m.length-1+it.parent.cloumn-it.cloumn:it.parent.cloumn
+    var p=pc>=0?m[pc][m[pc].length-1]:{row:[1],cloumn:-1}
+    while (compareRow(p.row,row)>0) p=p.head
+    op[op.length-1].parent=p
+    row=getFootRow(op[op.length-1],d)
+  }
+}
+function copyCloumn(m,b,t,c,i,ex,d){
+  var it=m[c][0]
+  m.push([])
+  while (true){
+    copyElement(m,b,t,it,m[m.length-1],i,d)
+    if ('foot' in it) it=it.foot
+    else break
+  }
+  m[m.length-1][m[m.length-1].length-1].value=ex.length?ex[m.length-1]:it.value
+  it=m[m.length-1][m[m.length-1].length-1]
+  while ('head' in it){
+    it.head.value=it.value+it.head.parent.value
+    it=it.head
+  }
+}
+function expandDimensionSequnece(m,b,t,n,d){
+  if (compareDimension(b.row,t.row)>=0) return
+  var s=getDs(m),c=getBootIndex(s,d)[0]
+  for (var i=b.cloumn+1;i<=t.cloumn;i++){
     for (var j=0;j<m[i].length;j++){
-      if (m[i][j].no!=b.no||rowDifference(m[i][j].row,b.row).length<b.row.length||m[i][j].row.length==1) continue
-      var c=getReferenceChain(m[i][j]).concat(getReferenceChain(t).slice(b.chain.length)).map(e=>{return {value:e.row.length}})
-      if (m[i][j].row.length==b.row.length) c=getReferenceChain(t).map(e=>{return {value:e.row.length}})
-      c[c.length-1].value+=1
-      for (var k=0;k<c.length;k++){
-        c[k].row=[0]
-        c[k].cloumn=k
-        if (c[k].value<=1) c[k].parent={row:[0],cloumn:-1}
-        if (k==getReferenceChain(m[i][j]).length) {c[k].parent=c[b.chain.length-1];continue}
-        for (var kk=k;kk>=0;kk--) if (c[kk].value<c[k].value){c[k].parent=c[kk];break}
+      var it=m[i][j],ds=s.slice(),pos=1
+      if (it.no!=b.no||compareDimension(rowDifference(it.row,it.ref.row),b.row)<=0||'foot' in it.ref&&compareRow(it.ref.foot.row,m[i][j].row)==0) continue
+      ds.splice(c+1,0,{value:rowDifference(it.row,it.ref.row)[1],parent:ds[c]})
+      while (it.ref.cloumn>b.cloumn){
+        it=it.ref
+        ds.splice(c+1,0,{value:rowDifference(it.row,it.ref.row)[1],parent:ds[c]})
+        ds[c+2].parent=ds[c+1]
+        pos++
       }
-      var len=c.length-b.chain.length,ex=[]
-      m[i][j].dim_seq=[]
-      for (var k=0;k<mm[0].length;k++) if (isEqualSequnce(c,mm[0][k])&&mm[1][k]>=n&&compareRow(mm[2][k],d)==0){ex=mm[3][k];break}
-      if (ex.length==0){
-        ex=expand(c,n,d)
-        mm[0].push(c)
-        mm[1].push(n)
-        mm[2].push(d)
-        mm[3].push(ex)
+      for (var k=0;k<ds.length;k++){
+        ds[k].cloumn=k
+        while (ds[k].value<=ds[k].parent.value) ds[k].parent=ds[k].parent.parent
       }
-      var idx=getReferenceChain(m[i][j]).length
-      if (m[i][j].row.length==b.row.length) idx=getReferenceChain(b).length
-      for (var k=1;k<=n;k++) m[i][j].dim_seq.push(ex[idx-1+len*k])
+      var dd=[0,1],len=ds.length-1-c
+      if (d.length>2&&d[0]==0&&d[1]==0) dd=d.slice(2)
+      if (d.length==3&&d[0]==1&&d[1]==0&&d[2]==1) dd=d
+      var ex=expand(ds,n,dd,inputm)
+      it=m[i][j]
+      it.offset=[]
+      for (var ii=0;ii<n;ii++){
+        var offset=[]
+        for (var jj=1;jj<=pos;jj++){
+          offset=rowAddition(offset,[1,ex[c+len*(ii+1)+jj]])
+        }
+        it.offset.push(offset)
+      }
     }
   }
 }
-function expand(s,n,d,f=false){
+function expand(s,n,d,f=true){
   if (s[s.length-1].value<=1) return s.slice(0,-1).map(e=>{return e.value})
-  var c=getBootCloumn(s,d)
-  var m=drawMountain(s,d),ex=[]
-  setElementRefrence(m)
+  var idx=getBootIndex(s,d),m=drawMountain(s,d),b=m[idx[0]][idx[1]],t=m[m.length-1][m[m.length-1].length-1],ex=[]
+  if (t.value==1) t=t.head
   if (f) displayMt(m)
-  var t=m[m.length-1][m[m.length-1].length-2],b=t.parent
-  if (c<b.cloumn&&c>=0) b=m[c][m[c].length-1]
-  var len=t.cloumn-b.cloumn
+  setElementRefrence(m)
   setElementNo(m,b)
-  if (d.length>2) {
-    b.chain=getReferenceChain(b)
-    t.parent.chain=getReferenceChain(t.parent)
-    expandElementDimensionSequence(m,b,t,n,d)
+  expandDimensionSequnece(m,b,t,n,d)
+  if (t.value-b.value>1&&proc(d).length==1){
+    var o=getOds(m)
+    ex=expand(o,n,d.length==1||divide(d).length==1?d:merge(divide(d).slice(1)),f)
   }
-  if (t.foot.value>1){
-    var o=[]
-    m.forEach(e=>{o.push({value:e[e.length-1].value,row:[0],cloumn:e[0].cloumn,parent:e[e.length-1].value==1?{row:[0],cloumn:-1}:o[e[e.length-1].parent.cloumn]})})
-    for (var i=0;i<m[m.length-1].length;i++) m[m.length-1][i].value--
-    ex=expand(o,n,d,true)
-  }
-  else {
+  else if ('foot' in t){
     m[m.length-1].pop()
     delete t.foot
-    for (var i=0;i<m[m.length-1].length;i++) m[m.length-1][i].value--
     t.parent=t.parent.parent
   }
+  for (var i=0;i<m[m.length-1].length;i++) m[m.length-1][i].value--
   for (var i=b.no;i<m[b.cloumn].length;i++){
-    m[t.cloumn].push({value:m[b.cloumn][i].value,row:m[b.cloumn][i].row,cloumn:t.cloumn,no:m[b.cloumn][i].no,parent:m[b.cloumn][i].parent,head:m[t.cloumn][m[t.cloumn].length-1],ref:m[b.cloumn][i].ref})
+    var idx=t.idx
+    m[t.cloumn].push({value:m[b.cloumn][i].value,row:m[b.cloumn][i].row,cloumn:t.cloumn,idx:idx++,no:m[b.cloumn][i].no,parent:m[b.cloumn][i].parent,head:m[t.cloumn][m[t.cloumn].length-1],ref:m[b.cloumn][i].ref})
     m[t.cloumn][m[t.cloumn].length-2].foot=m[t.cloumn][m[t.cloumn].length-1]
   }
   for (var i=0;i<n;i++){
     for (var j=b.cloumn+1;j<=t.cloumn;j++){
-      var l=m.length,op=[],max_row
-      m.push(op)
-      for (var k=0;k<m[j].length;k++){
-        var head={row:[0],cloumn:l,no:m[j][0].no,parent:m[j][0].parent}
-        if ('dim_seq' in m[j][k]){
-          max_row=m[j][k].row.slice()
-          max_row[0]+=t.row[0]-1
-          while (max_row.length<m[j][k].dim_seq[i]) max_row.splice(1,0,0)
-        }
-        else if (m[j][k].no>0){
-          for (var kk=m[b.cloumn+len*(i+1)].length-1;kk>=0;kk--){
-            if (m[b.cloumn+len*(i+1)][kk].no>m[j][k].no) continue
-            max_row=rowAddition(m[b.cloumn+len*(i+1)][kk].row,rowDifference(m[j][k].row,m[b.cloumn][m[j][k].no-1].row))
-            break
-          }
-        }
-        else max_row=m[j][k].row
-        if (compareRow(d,[0])==0) max_row=m[j][k].row
-        if (op.length){
-          head={row:calcFootRow(op[op.length-1],d),cloumn:l,no:m[j][k].no,parent:m[j][k].parent,head:op[op.length-1]}
-          op[op.length-1].foot=head
-        }
-        if (m[j][k].value==1&&m[j][k].row[0]>0) head.parent=m[j][k].ref
-        op.push(head)
-        if (head.parent.cloumn>=b.cloumn) head.parent=m[head.parent.cloumn+len*i+len][m[head.parent.cloumn+len*i+len].length-1]
-        while (compareRow(head.parent.row,head.row)>0) head.parent=head.parent.head
-        copyElement(op,op[op.length-1],max_row,d)
-      }
-      op[op.length-1].value=ex.length?ex[j+len*i+len]:m[j][m[j].length-1].value
-      for (var k=m[l].length-1;k>0;k--) m[l][k-1].value=m[l][k].value+m[l][k-1].parent.value
+      copyCloumn(m,b,t,j,i,ex,d)
     }
   }
-  if (f) displayMt(m,false)
+  if (f) displayMt(m)
   return m.map(e=>{return e[0].value})
 }
 function toSequence(s){
   var seq=[]
   for (var i=0;i<s.length;i++){
-    if (s[i]<=1) {seq.push({value:s[i],row:[0],cloumn:i,parent:{row:[0],cloumn:-1}});continue}
-    for (var j=i-1;j>=0;j--) if (s[j]<s[i]) {seq.push({value:s[i],row:[0],cloumn:i,parent:seq[j]});break}
+    if (s[i]<=1) {seq.push({value:s[i],cloumn:i,parent:{row:[1],cloumn:-1}});continue}
+    for (var j=i-1;j>=0;j--) if (s[j]<s[i]) {seq.push({value:s[i],cloumn:i,parent:seq[j]});break}
   }
   return seq
 }
 //Limited to n<=10
-function expandmultilimited(s,nstring,dstring){
+function expandmultilimited(s,nstring,dstring,){
   var result=s;
-  for (var i of nstring.split(",")) result=expand(toSequence(result.split(itemSeparatorRegex).map(e=>{return Number(e)})),Math.min(i,10),dstring.split(itemSeparatorRegex).map(e=>{return Number(e)}),true).toString();
+  for (var i of nstring.split(",")) result=expand(toSequence(result.split(itemSeparatorRegex).map(e=>{return Number(e)})),Math.min(i,10),dstring.split(itemSeparatorRegex).map(e=>{return Number(e)})).toString();
   return result;
 }
 var input="";
 var inputn="3";
 var inputd="0";
+var inputm="true";
 var mt="";
 var mm=[[],[],[],[]];
 function expandall(){
-  if (input==dg("input").value&&inputn==dg("inputn").value&&inputd==dg("inputd").value) return;
+  if (input==dg("input").value&&inputn==dg("inputn").value&&inputd==dg("inputd").value&&inputm==dg("inputm").checked) return;
   input=dg("input").value;
   inputn=dg("inputn").value;
   inputd=dg("inputd").value;
+  inputm=dg("inputm").checked;
   mt="";
   dg("output").value=input.split(lineBreakRegex).map(e=>expandmultilimited(e,inputn,inputd)).join("\n");
   dg("mt").innerHTML=mt
@@ -292,7 +315,7 @@ window.onpopstate=function (e){
 }
 function load(){}
 var handlekey=function(e){
-  setTimeout(expandall,0,true);
+  //setTimeout(expandall,0,true);
 }
 //console.log=function (s){alert(s)};
 window.onerror=function (e,s,l,c,o){alert(JSON.stringify(e+"\n"+s+":"+l+":"+c+"\n"+o.stack))}
